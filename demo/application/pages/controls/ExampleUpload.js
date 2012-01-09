6b6b228b0aaa520834446d066b2be48188f9e042
gillis <gillis@arnold.vivesta.local>
2012-01-09 15:44:24 +0100

    updated xhr upload control

--- demo/application/pages/controls/ExampleUpload.js
+++ demo/application/pages/controls/ExampleUpload.js
@@ -14,9 +14,11 @@ namespace('Application.Controls.Examples').ExampleUpload = Application.Controls.
 	
 	createComponents : function()
 	{
-		var upload = new Banana.Controls.Upload();
-		upload.setUploadUrl("upload.php");
-		
+		var upload = new Banana.Controls.ChunkedUpload();
+		upload.setMultipleFilesUpload(true);
+		upload.setChunkSize(2000000);
+		upload.setPostUrl("upload.php");
+
 		upload.bind("filesSelected",this.getProxy(function(e,data){
 			this.createFileProgression(data.files);
 		}));
@@ -27,6 +29,16 @@ namespace('Application.Controls.Examples').ExampleUpload = Application.Controls.
 		
 		upload.bind('fileUploaded',this.getProxy(function(e,data){
 			this.finishFileIndicator(data.file);
+		}));
+
+		upload.bind('fileUploadError',this.getProxy(function(e,data){
+			alert('error')
+			this.errorFileIndicator(data.file);
+		}));
+
+		upload.bind("browserNotSupported",this.getProxy(function(e,data){
+		
+			this.addControl("Chunk upload not supported by the browser");
 		}));
 		
 		
@@ -71,6 +83,12 @@ namespace('Application.Controls.Examples').ExampleUpload = Application.Controls.
 	{	
 		var indicator = this.fileIndicators[file.name];
 		indicator.setCss({"background-color":"green"});
+	},
+
+	errorFileIndicator : function(file)
+	{
+		var indicator = this.fileIndicators[file.name];
+		indicator.setCss({"background-color":"red"});
 	}
 });
 
@@ -93,25 +111,53 @@ namespace('Application.Controls.Examples').ExampleUpload = Application.Controls.
 
 
 
-
+/** @namespace Banana.Controls.Form */
 namespace('Banana.Controls').Form = Banana.UiControl.extend({
-	
+/** @lends Banana.Controls.Form.prototype */
+
+	/**
+	 * Creates form control
+	 * @constructs
+	 * @extends Banana.UiControl
+	 */
+	init : function()
+	{
+		this._super();
+	},
+
+	/**
+	 * @ignore
+	 */
 	getTagName : function()
 	{
 		return 'form';
 	}	
 });
 
+/** @namespace Banana.Controls.FileInput*/
 namespace('Banana.Controls').FileInput = Banana.Controls.InputControl.extend({
-	
+/** @lends Banana.Controls.FileInput.prototype */
+
+	/**
+	 * Creates file input control
+	 * @constructs
+	 * @extends Banana.UiControl
+	 */
 	init : function()
 	{
 		this._super();
 		
 		this.setAttribute("type",'file');
-		this.setAttribute("multiple",'true');
 	},
-	
+
+	/**
+	 * @param {boolean}
+	 */
+	setMultiple : function(bool)
+	{
+		this.setAttribute("multiple",bool);
+	},
+
 	/**
 	 * @return {Array} of files
 	 */
@@ -122,20 +168,27 @@ namespace('Banana.Controls').FileInput = Banana.Controls.InputControl.extend({
 	}
 });
 
-namespace('Banana.Controls').FileInputFile = Banana.Control.extend({
-	
-	
-});
+/** @namespace Banana.Controls.ChunkUpload*/
+namespace('Banana.Controls').ChunkedUpload = Banana.Controls.Panel.extend({
+/** @lends Banana.Controls.ChunkUpload.prototype */
 
-namespace('Banana.Controls').Upload = Banana.Controls.Panel.extend({
-	
+	/**
+	 * creates a chunked upload control
+	 * currently only supported by chrome and firefox.
+	 * uploads file is small chunks. the upload control ensures that chunks are send to the
+	 * server in the correct order. Server is responsible to reconstruct the chunks to one file
+	 * first time a file is uploaded we
+	 * @constructs
+	 * @extends Banana.Controls.Panel
+	 */
 	init : function()
 	{
 		this._super();
 	
-		if (!this.supportAjaxUploadProgressEvents())
+		if (!this.isSupported())
 		{
-			this.addControl("chunk upload not supported by your browser");
+			this.triggerError = true;
+			log.error("chunk upload not supported by the browser");
 			return;
 		}
 		
@@ -144,7 +197,6 @@ namespace('Banana.Controls').Upload = Banana.Controls.Panel.extend({
 		this.previousLoadedChunk = {};
 		
 		this.chunkSize = 2000000;
-		this.uploadFile = 'upload/upload.php';
 		///this.uploadFile = 'http://192.168.250.72/~gillis/upload.php';
 		
 		var form = new Banana.Controls.Form();
@@ -160,6 +212,7 @@ namespace('Banana.Controls').Upload = Banana.Controls.Panel.extend({
 		fileInput.bind('change',this.getProxy(function(){
 			this.handleFilesSelected(fileInput.getFiles());
 		}));
+		fileInput.setId('fileInput');
 		 
 		form.addControl(maxInput);
 		form.addControl(fileInput);
@@ -167,13 +220,48 @@ namespace('Banana.Controls').Upload = Banana.Controls.Panel.extend({
 		this.addControl(form);	
 	},
 
+	createComponents : function()
+	{
+		if (this.triggerError)
+		{
+			this.triggerEvent("browserNotSupported");
+		}
+	},
+
+	/**
+	 * set the size of a chunk. the lower you set the chunk, the lower memory usuage will be, but also
+	 * 
+	 * @param {int} chunkSize
+	 */
+	setChunkSize : function(chunkSize)
+	{
+		this.chunkSize = chunkSize;
+	},
+
+	setMultipleFilesUpload : function(bool)
+	{
+		var control = this.findControl('fileInput');
+		if (!control)
+		{
+			return;
+		}
+		control.setMultiple(bool);
+	},
+
+	/**
+	 * sets the url to post to
+	 * @param {String} url
+	 */
 	setPostUrl : function(url)
 	{
 		this.uploadFile = url;
 		return this; 
 	},
-	
-	supportAjaxUploadProgressEvents : function() 
+
+	/**
+	 * Check whether our upload control is supported by the browser
+	 */
+	isSupported : function()
 	{
 		if (!window.File)
 		{
@@ -196,7 +284,11 @@ namespace('Banana.Controls').Upload = Banana.Controls.Panel.extend({
 		this.triggerEvent("filesSelected",{"files":files});
 		this.uploadFiles(files);
 	},
-	
+
+	/**
+	 * Executes the upload procedure
+	 * @param {Array} of files
+	 */
 	uploadFiles : function(files)
 	{
 		this.previousLoadedChunk = {};
@@ -206,136 +298,150 @@ namespace('Banana.Controls').Upload = Banana.Controls.Panel.extend({
 		{
 			var file = files[i];
 			file.loaded = 0;
+			file.uid = Banana.Util.generateUniqueId();
 			file.completion = 0;
 			
 			this.files[file.name] = file;
-			
+
 			this.processFileChunkFrom(file,0,null);
 		}
 	},
-	
+
+	/**
+	 * Processes a chunk from a file
+	 * this method is recalled everytime a chunk is completed.
+	 * it stops when all chunks are completed
+	 * @param file
+	 * @param index part of the file chunk
+	 * @param cb called after each upload chunk completion
+	 */
 	processFileChunkFrom : function(file,index,cb)
 	{
 		var start = file.loaded;
 		var end = file.loaded+this.chunkSize;
-		
-		if (index != 0)
+		var lastChunk = false;
+
+		//if we have an error in the file. we trigger event and stop processing
+		if (file.uploadError)
 		{
-			//start +=1;
-			//end+=1;
+			this.triggerEvent("fileUploadError",{"file":file});
+			return;
 		}
-		if (index == 1)
-		{
-			
-		}
-		
+
+		//we always end up here when all chunks are uploaded
 		if (start >= file.size)
 		{
-			//console.log("file ",file.name,"finished ",start);
 			this.triggerEvent("fileUploaded",{"file":file});
 			return;
 		}
-		
+
+		//we end up here at the last chunk
 		if (end >= file.size)
 		{
 			end = file.size;
+			lastChunk = true;
 		}
-		
-		
-		
+
+		//slice method depending on the browser
 		var slice = file.webkitSlice || file.mozSlice || file.slice
-		
+
+		//get a chunk from the file
 		var chunk = slice.call(file,start,end);
-	
-//		var chunk = null;
-//		if (file.webkitSlice)
-//		{
-//			chunk = file.webkitSlice(start,end);
-//		}
-//		else if (file.mozSlice)
-//		{
-//			chunk = file.mozSlice(start,end);
-//		}  
-			
-		//console.log("upload",chunk," from ",(start),"to",(end)," filesize  "+file.size," chunksize ",chunk.size);
-		
+
 		chunk.filename = file.name;
 		chunk.index = index;
-	
+
+		//below we have a callback function encapsulated in a directly executed function
+		//we do this to ensure a closure of where the file resists in.
+		//the situation can occur that we upload 2 different files at the same time. we want the
+		//callback function access the right file always. without the closure the callback will always access the
+		//last set file, which can be wrong
 		var $this = this;
 		
-		this.uploadChunk(chunk,file,function(index,file,cb)
+		this.uploadChunk(chunk,file,lastChunk,function(index,file,cb)
 		{
 			var func = function(data)
 			{
-				
 				file = this.getFile(file.name);
 				
 				file.loaded += data.loaded;
 				file.completion = (file.loaded/file.size*100);
-				//console.log('file',file,'  for file ',file.name,'finished. file size: ',file.size,'file loaded',file.loaded,'perc',(file.loaded/file.size*100))
-				this.previousLoadedChunk[file.name] = 0;
 				
 				this.triggerEvent("fileUploading",{"file":file});
 				this.processFileChunkFrom(file,++index,cb);
-			
 			}
 			
 			return jQuery.proxy( func, $this);
 			//alert("yes")
 		}(index,file,cb));
 	},
-	
+
+	/**
+	 * @param String filename
+	 * @return {Object}
+	 */
 	getFile : function(filename)
 	{
 		return this.files[filename];
 	},
-	
-	uploadChunk : function(chunk,file,cb)
+
+	/**
+	 * Uploads a chunk
+	 * if a chunk fails we set uploadError on file object to true
+	 * @param {Object} chunk
+	 * @param {Object} file
+	 * @param cb fired after successfully uploading chunk
+	 */
+	uploadChunk : function(chunk,file,lastChunk,cb)
 	{
 		var fd = new FormData();
 		
 		fd.append("chunk",chunk);
 		fd.append("filename",chunk.filename);
+		fd.append("uid",file.uid);
 		
 		if (chunk.index == 0)
 		{
-			fd.append("removeFile",1);
+			fd.append("firstChunk",1);
 		}
-		
+
+		if ((file.loaded +this.chunkSize) >= file.size)
+		{
+			fd.append("lastChunk",1);
+		}
+
 		var xhr = new XMLHttpRequest();
 		
-		xhr.addEventListener("error",this.getProxy(function(e){
-			
-			alert("error")
-		})); 
+		xhr.addEventListener("error",this.getProxy(function(e){}));
 
 		var $this = this;
-		
+
+		xhr.addEventListener("readystatechange",function(file){
+
+			var func = function(e,f)
+			{
+				if (e.target.readyState == 4)
+				{
+					if (e.target.status != 200)
+					{
+						file.uploadError = true;
+					}
+				}
+			}
+			return jQuery.proxy( func, $this);
+
+		}(file));
+
 		xhr.upload.addEventListener("progress",function(file){
 						
 			var func = function(e,f)
 			{
 				file = this.getFile(file.name);
-				//console.log(e,f);
-				//var file = this.getFile()
-				//console.log('chunk for file ',file.name,' progress',"filesize",file.size,"chunkloaded",e.loaded,"chunktotal",e.total);	
-				
-				if (!this.previousLoadedChunk[file.name])
-				{
-					this.previousLoadedChunk[file.name] = 0;
-				}
-				
-				var added = e.loaded - this.previousLoadedChunk[file.name];
-				//console.log('add ',added);
+
 				var newPerc = ((file.loaded+e.loaded) / file.size) * 100
 				file.completion = Math.max(0,Math.min(newPerc,100));
-				console.log('new perc ',newPerc);
-				//file.loaded += added - file.name.length;
-				this.previousLoadedChunk[file.name] +=added;
-				
+
 				this.triggerEvent("fileUploading",{"file":file});
-				//this.triggerEvent("fileUploading",{"file":file});
 			}
 			
 			return jQuery.proxy( func, $this);
--- demo/upload.php
+++ demo/upload.php
@@ -3,63 +3,129 @@
 
 class ChunkUpload
 {
+	private $_targetPath;
+	private $_tempName;
+	private $_tempPath;
 	private $_target;
 	private $_source;
-	private $_logHandle;
 
-	public function __construct()
-	{
-		$this->_logHandle = fopen("log.txt",'a'); 
-	}
-
+	/**
+	 * The source chunk file
+	 * @param $source
+	 * @return void
+	 */
 	public function setSource($source)
 	{
 		$this->_source = $source;
 	}
-	
+
+	/**
+	 * Path where the chunk file should be constructed from other chunks
+	 * @param $path
+	 * @return void
+	 */
+	public function setTempPath($path)
+	{
+		$this->_tempPath = $path;
+	}
+
+	/**
+	 * Path where the file will be copied when all chunks are complete
+	 * @param $path
+	 * @return void
+	 */
+	public function setTargetPath($path)
+	{
+		$this->_targetPath = $path;
+	}
+
+	/**
+	 * The name of the temporary file used to construct the file from chunks
+	 * @param $name
+	 * @return void
+	 */
+	public function setTempName($name)
+	{
+		$this->_tempName = $name;
+	}
+
+	/**
+	 * Name of the final file.
+	 * @param $target
+	 * @return void
+	 */
 	public function setTarget($target)
 	{
 		$this->_target = $target;
 	}
-	
-	public function removeTarget()
+
+
+	/**
+	 * build to file in Temp
+	 * @return void
+	 */
+	public function build()
 	{
-		unlink($this->_target);
-	}
-	
-	public function appendToFile()
-	{
-		$target = fopen($this->_target,"a+b");
-		
-		fwrite($target, file_get_contents($this->_source));
+		$target = fopen($this->_tempPath."/".$this->_tempName,"a+b");
+
+		$btw = filesize($this->_source);
+
+		$wb = fwrite($target, file_get_contents($this->_source));
+
 		fclose($target);
+
+		if ($btw != $wb)
+		{
+			header('HTTP/1.1 500 Internal Server Error');
+		}
 	}
-	
-	public function close()
+
+	/**
+	 * Removes the target file. Use this at the beginning
+	 * @return void
+	 */
+	public function destroyTarget()
 	{
-		fclose($this->_logHandle);
+		if (file_exists($this->_targetPath.$this->_target))
+		{
+			unlink($this->_targetPath.$this->_target);
+		}
 	}
-	
-	public function log($string)
+
+	/**
+	 * Creates a file from the constructed temp file.
+	 * usually this method should be called after the last chunk is build
+	 * @return void
+	 */
+	public function buildTarget()
 	{
-		fwrite($this->_logHandle, $string);
+		rename($this->_tempPath.$this->_tempName,$this->_targetPath.$this->_target);
 	}
 }
 
-
+ 
 
 $c = new ChunkUpload();
-
+$c->setTargetPath("upload/");
+$c->setTempPath("/tmp/");
+$c->setTempName("CHUNKUP_".$_POST['uid']);
 $c->setSource($_FILES['chunk']['tmp_name']);
 $c->setTarget($_POST['filename']);
 
-if (isset($_POST['removeFile']))
+
+//if we have the first chunk, we remove the file (if exists)
+if (isset($_POST['firstChunk']))
 {
-	$c->removeTarget();
+	$c->destroyTarget();
 }
 
-$c->appendToFile();
-$c->close();
+//append chunks to target
+$c->build();
+
+if (isset($_POST['lastChunk']))
+{
+	$c->buildTarget();
+}
 
 return $_POST['filename'];
 ?>
